4ee0920527ef0912edcafd966b2f22930d6be573
Maliha Binte Masud <maliha.binte.masud@g.bracu.ac.bd>
2025-01-06 03:53:00 +0600

    genjam sort

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,19 +1,12 @@
 import express from 'express'
-<<<<<<< Updated upstream
-import { login, verify } from '../controllers/authController.js'
-=======
 import { login, verify, getUsers, logout } from '../controllers/authController.js'
->>>>>>> Stashed changes
 import authMiddleware from '../middleware/authMiddlware.js'
 
 const router = express.Router()
 
 router.post('/login', login)
 router.get('/verify', authMiddleware, verify)
-<<<<<<< Updated upstream
-=======
 router.get('/getUsers', getUsers)
 router.post('/logout', authMiddleware, logout)
->>>>>>> Stashed changes
 
 export default router;
